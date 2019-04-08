ccae731a7afeea1e85223022c77143a9feb924f3
dyike <yuanfeng634@gmail.com>
2019-04-08 17:07:57 +0800

    fix log

--- nginx-1.15.8/src/ngx_http_mytest_module/ngx_http_mytest_module.c
+++ nginx-1.15.8/src/ngx_http_mytest_module/ngx_http_mytest_module.c
@@ -76,7 +76,7 @@ static ngx_int_t ngx_http_mytest_handler(ngx_http_request_t *r)
 
     // set response header 
     ngx_str_t type = ngx_string("text/plain");
-    ngx_str_t response = ngx_string("hi, this message comes from bili dev server");
+    ngx_str_t response = ngx_string("{\"code\":0,\"msg\":\"ok\"}");
     r->headers_out.status = NGX_HTTP_OK;
     r->headers_out.content_length_n = response.len;
     r->headers_out.content_type = type;
